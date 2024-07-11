d2b5e55eccd4fb8f8df376600f4f87b0df94f6be
mac <maciek@kukuczka.net>
2024-07-11 19:36:43 +0200

    Add new DB

---
+++ Docs/Data/Migrations/20240709003830_Init.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Docs.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Descritpion = table.Column<string>(type: "TEXT", nullable: true),
+                    IsActive = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ModifyDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DocPaths",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Path = table.Column<string>(type: "TEXT", nullable: false),
+                    IsActive = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ModifyDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DocPaths", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Docs",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    ShortDescription = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Path_CreateDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Path_Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Path_IsActive = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Path_ModifyDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Path_Path = table.Column<string>(type: "TEXT", nullable: false),
+                    IsActive = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ModifyDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Docs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    ImagePath = table.Column<string>(type: "TEXT", nullable: false),
+                    IsActive = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ModifyDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Links",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Url = table.Column<string>(type: "TEXT", nullable: false),
+                    IsActive = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ModifyDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Links", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Content = table.Column<string>(type: "TEXT", nullable: true),
+                    IsActive = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ModifyDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    IsActive = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ModifyDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CategoryDoc",
+                columns: table => new
+                {
+                    CategoriesId = table.Column<string>(type: "TEXT", nullable: false),
+                    DocsId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryDoc", x => new { x.CategoriesId, x.DocsId });
+                    table.ForeignKey(
+                        name: "FK_CategoryDoc_Categories_CategoriesId",
+                        column: x => x.CategoriesId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CategoryDoc_Docs_DocsId",
+                        column: x => x.DocsId,
+                        principalTable: "Docs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DocDocPath",
+                columns: table => new
+                {
+                    DocsId = table.Column<string>(type: "TEXT", nullable: false),
+                    RelatedDocsId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DocDocPath", x => new { x.DocsId, x.RelatedDocsId });
+                    table.ForeignKey(
+                        name: "FK_DocDocPath_DocPaths_RelatedDocsId",
+                        column: x => x.RelatedDocsId,
+                        principalTable: "DocPaths",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DocDocPath_Docs_DocsId",
+                        column: x => x.DocsId,
+                        principalTable: "Docs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DocImage",
+                columns: table => new
+                {
+                    DocsId = table.Column<string>(type: "TEXT", nullable: false),
+                    ImagesId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DocImage", x => new { x.DocsId, x.ImagesId });
+                    table.ForeignKey(
+                        name: "FK_DocImage_Docs_DocsId",
+                        column: x => x.DocsId,
+                        principalTable: "Docs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DocImage_Images_ImagesId",
+                        column: x => x.ImagesId,
+                        principalTable: "Images",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DocLink",
+                columns: table => new
+                {
+                    DocsId = table.Column<string>(type: "TEXT", nullable: false),
+                    LinksId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DocLink", x => new { x.DocsId, x.LinksId });
+                    table.ForeignKey(
+                        name: "FK_DocLink_Docs_DocsId",
+                        column: x => x.DocsId,
+                        principalTable: "Docs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DocLink_Links_LinksId",
+                        column: x => x.LinksId,
+                        principalTable: "Links",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DocNote",
+                columns: table => new
+                {
+                    DocsId = table.Column<string>(type: "TEXT", nullable: false),
+                    NotesId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DocNote", x => new { x.DocsId, x.NotesId });
+                    table.ForeignKey(
+                        name: "FK_DocNote_Docs_DocsId",
+                        column: x => x.DocsId,
+                        principalTable: "Docs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DocNote_Notes_NotesId",
+                        column: x => x.NotesId,
+                        principalTable: "Notes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DocTag",
+                columns: table => new
+                {
+                    DocsId = table.Column<string>(type: "TEXT", nullable: false),
+                    TagsId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DocTag", x => new { x.DocsId, x.TagsId });
+                    table.ForeignKey(
+                        name: "FK_DocTag_Docs_DocsId",
+                        column: x => x.DocsId,
+                        principalTable: "Docs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DocTag_Tags_TagsId",
+                        column: x => x.TagsId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryDoc_DocsId",
+                table: "CategoryDoc",
+                column: "DocsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DocDocPath_RelatedDocsId",
+                table: "DocDocPath",
+                column: "RelatedDocsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DocImage_ImagesId",
+                table: "DocImage",
+                column: "ImagesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DocLink_LinksId",
+                table: "DocLink",
+                column: "LinksId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DocNote_NotesId",
+                table: "DocNote",
+                column: "NotesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DocTag_TagsId",
+                table: "DocTag",
+                column: "TagsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CategoryDoc");
+
+            migrationBuilder.DropTable(
+                name: "DocDocPath");
+
+            migrationBuilder.DropTable(
+                name: "DocImage");
+
+            migrationBuilder.DropTable(
+                name: "DocLink");
+
+            migrationBuilder.DropTable(
+                name: "DocNote");
+
+            migrationBuilder.DropTable(
+                name: "DocTag");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "DocPaths");
+
+            migrationBuilder.DropTable(
+                name: "Images");
+
+            migrationBuilder.DropTable(
+                name: "Links");
+
+            migrationBuilder.DropTable(
+                name: "Notes");
+
+            migrationBuilder.DropTable(
+                name: "Docs");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+        }
+    }
+}
---
+++ Docs/Data/Migrations/20240709084319_Init2.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Docs.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Path_CreateDate",
+                table: "Docs");
+
+            migrationBuilder.DropColumn(
+                name: "Path_Id",
+                table: "Docs");
+
+            migrationBuilder.DropColumn(
+                name: "Path_IsActive",
+                table: "Docs");
+
+            migrationBuilder.DropColumn(
+                name: "Path_ModifyDate",
+                table: "Docs");
+
+            migrationBuilder.DropColumn(
+                name: "Path_Path",
+                table: "Docs");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifyDate",
+                table: "Tags",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifyDate",
+                table: "Notes",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifyDate",
+                table: "Links",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifyDate",
+                table: "Images",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifyDate",
+                table: "Docs",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifyDate",
+                table: "DocPaths",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DocId",
+                table: "DocPaths",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifyDate",
+                table: "Categories",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DocPaths_DocId",
+                table: "DocPaths",
+                column: "DocId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DocPaths_Docs_DocId",
+                table: "DocPaths",
+                column: "DocId",
+                principalTable: "Docs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DocPaths_Docs_DocId",
+                table: "DocPaths");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DocPaths_DocId",
+                table: "DocPaths");
+
+            migrationBuilder.DropColumn(
+                name: "DocId",
+                table: "DocPaths");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifyDate",
+                table: "Tags",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifyDate",
+                table: "Notes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifyDate",
+                table: "Links",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifyDate",
+                table: "Images",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifyDate",
+                table: "Docs",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Path_CreateDate",
+                table: "Docs",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Path_Id",
+                table: "Docs",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Path_IsActive",
+                table: "Docs",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Path_ModifyDate",
+                table: "Docs",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Path_Path",
+                table: "Docs",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifyDate",
+                table: "DocPaths",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifyDate",
+                table: "Categories",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
