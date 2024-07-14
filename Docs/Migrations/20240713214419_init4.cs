858affe17e4e561dd57d07b3eeb958f60dc9706c
mac <maciek@kukuczka.net>
2024-07-14 03:02:52 +0200

    Add initial database migration script
    This commit introduces the initial migration for setting up the database schema. It includes the creation of tables such as AspNetRoles, Categories, Docs, and various relationships among them. Additionally, it adds an app.db file to Docs\Data.

---
+++ Docs/Migrations/20240713214419_init4.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Docs.Migrations
+{
+    /// <inheritdoc />
+    public partial class init4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplicationUserSubject");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Subjects",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subjects_UserId",
+                table: "Subjects",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Subjects_AspNetUsers_UserId",
+                table: "Subjects",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Subjects_AspNetUsers_UserId",
+                table: "Subjects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Subjects_UserId",
+                table: "Subjects");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Subjects");
+
+            migrationBuilder.CreateTable(
+                name: "ApplicationUserSubject",
+                columns: table => new
+                {
+                    SubjectsId = table.Column<string>(type: "TEXT", nullable: false),
+                    UsersId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationUserSubject", x => new { x.SubjectsId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_ApplicationUserSubject_AspNetUsers_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ApplicationUserSubject_Subjects_SubjectsId",
+                        column: x => x.SubjectsId,
+                        principalTable: "Subjects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationUserSubject_UsersId",
+                table: "ApplicationUserSubject",
+                column: "UsersId");
+        }
+    }
+}
