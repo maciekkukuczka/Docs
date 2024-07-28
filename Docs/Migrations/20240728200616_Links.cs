c16dfdae521039c212455353fc3bbc944a2eb8b4
mac <maciek@kukuczka.net>
2024-07-28 23:44:18 +0200

    Add migration for Links table
    Introduced new migration to add 'Name' and 'Description' columns to the 'Links' table. This migration also includes associated designer file modifications to support the schema changes.

---
+++ Docs/Migrations/20240728200616_Links.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Docs.Migrations
+{
+    /// <inheritdoc />
+    public partial class Links : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Links",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Links",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Links");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Links");
+        }
+    }
+}
