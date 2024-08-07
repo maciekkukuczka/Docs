cf3f06dd3f0e40d3871fee71f3e19313fcd64ab5
mac <maciek@kukuczka.net>
2024-08-07 13:23:18 +0200

    Remove unused initial migration file
    Deleted unnecessary migration file `20240802001427_Init.cs` and its associated designer file to clean up the project. This helps in keeping the repository tidy and reduces confusion.

--- Docs/Config/Usings.cs
+++ Docs/Config/Usings.cs
@@ -1,11 +1,15 @@
 ﻿// SYSTEM
+
 global using Microsoft.AspNetCore.Components.Authorization;
 global using Microsoft.AspNetCore.Identity;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 global using System.ComponentModel.DataAnnotations;
 global using System.Security.Claims;
+
+#if NET9
 global using Microsoft.Extensions.Caching.Hybrid;
+#endif
 global using System.Linq.Expressions;
 global using System.Text.Json.Serialization;
 global using Microsoft.AspNetCore.Components;
@@ -29,26 +33,18 @@ global using Serilog.Sinks.SystemConsole.Themes;
 
 // MODULES
 global using Mac.Modules.DataSeed;
-
 global using Docs.Modules.Generic.Services;
-
-global using  Mac.Modules.Common;
+global using Mac.Modules.Common;
 global using Docs.Modules.Common.Models;
 global using Docs.Modules.Common.Result;
-
 global using Docs.Modules.Items.Models;
 global using Docs.Modules.Items.Models.ViewModels;
 global using Docs.Modules.Items.Services;
-
 global using Docs.Modules.Subjects.Models;
 global using Docs.Modules.Subjects.Models.ViewModels;
 global using Docs.Modules.Subjects.Services;
-
 global using Docs.Modules.Categories.Models;
 global using Docs.Modules.Categories.Models.ViewModels;
 global using Docs.Modules.Categories.Services;
-
 global using Docs.Modules.Links.Models;
 global using Docs.Modules.Links.Services;
-
-
